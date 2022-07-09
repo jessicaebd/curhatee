db5079f5927f4c4dabe65f0e3e4183179f91d643
raynardtjhang <raytjhang@gmail.com>
2022-07-09 15:03:05 +0700

    remove seeder

--- database/seeders/TransactionSeeder.php
+++ database/seeders/TransactionSeeder.php
@@ -19,48 +19,48 @@ class TransactionSeeder extends Seeder
     public function run()
     {
         //
-        Transaction::create([
-            'user_id' => User::all()->random()->id,
-            'psychologist_id' => Psychologist::all()->random()->id,
-            'schedule_id' => Schedule::all()->random()->id,
-            'payment_type_id' => PaymentType::all()->random()->id,
-            'price' => 100000,
-            'detail' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit, illum optio. Enim autem iste qui voluptatem eum beatae suscipit mollitia ipsum nam molestiae ex, sint culpa porro provident rerum dolor?',
-            'status' => 'done',
-            'time' => '2022-06-28'
-        ]);
+        // Transaction::create([
+        //     'user_id' => User::all()->random()->id,
+        //     'psychologist_id' => Psychologist::all()->random()->id,
+        //     'schedule_id' => Schedule::all()->random()->id,
+        //     'payment_type_id' => PaymentType::all()->random()->id,
+        //     'price' => 100000,
+        //     'detail' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit, illum optio. Enim autem iste qui voluptatem eum beatae suscipit mollitia ipsum nam molestiae ex, sint culpa porro provident rerum dolor?',
+        //     'status' => 'done',
+        //     'time' => '2022-06-28'
+        // ]);
 
-        Transaction::create([
-            'user_id' => User::all()->random()->id,
-            'psychologist_id' => Psychologist::all()->random()->id,
-            'schedule_id' => Schedule::all()->random()->id,
-            'payment_type_id' => PaymentType::all()->random()->id,
-            'price' => 200000,
-            'detail' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit, illum optio. Enim autem iste qui voluptatem eum beatae suscipit mollitia ipsum nam molestiae ex, sint culpa porro provident rerum dolor?',
-            'status' => 'done',
-            'time' => '2022-05-29'
-        ]);
+        // Transaction::create([
+        //     'user_id' => User::all()->random()->id,
+        //     'psychologist_id' => Psychologist::all()->random()->id,
+        //     'schedule_id' => Schedule::all()->random()->id,
+        //     'payment_type_id' => PaymentType::all()->random()->id,
+        //     'price' => 200000,
+        //     'detail' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit, illum optio. Enim autem iste qui voluptatem eum beatae suscipit mollitia ipsum nam molestiae ex, sint culpa porro provident rerum dolor?',
+        //     'status' => 'done',
+        //     'time' => '2022-05-29'
+        // ]);
 
-        Transaction::create([
-            'user_id' => User::all()->random()->id,
-            'psychologist_id' => Psychologist::all()->random()->id,
-            'schedule_id' => Schedule::all()->random()->id,
-            'payment_type_id' => PaymentType::all()->random()->id,
-            'price' => 200000,
-            'detail' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit, illum optio. Enim autem iste qui voluptatem eum beatae suscipit mollitia ipsum nam molestiae ex, sint culpa porro provident rerum dolor?',
-            'status' => 'done',
-            'time' => '2022-04-28'
-        ]);
+        // Transaction::create([
+        //     'user_id' => User::all()->random()->id,
+        //     'psychologist_id' => Psychologist::all()->random()->id,
+        //     'schedule_id' => Schedule::all()->random()->id,
+        //     'payment_type_id' => PaymentType::all()->random()->id,
+        //     'price' => 200000,
+        //     'detail' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit, illum optio. Enim autem iste qui voluptatem eum beatae suscipit mollitia ipsum nam molestiae ex, sint culpa porro provident rerum dolor?',
+        //     'status' => 'done',
+        //     'time' => '2022-04-28'
+        // ]);
 
-        Transaction::create([
-            'user_id' => User::all()->random()->id,
-            'psychologist_id' => Psychologist::all()->random()->id,
-            'schedule_id' => Schedule::all()->random()->id,
-            'payment_type_id' => PaymentType::all()->random()->id,
-            'price' => 100000,
-            'detail' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit, illum optio. Enim autem iste qui voluptatem eum beatae suscipit mollitia ipsum nam molestiae ex, sint culpa porro provident rerum dolor?',
-            'status' => 'done',
-            'time' => '2022-05-27'
-        ]);
+        // Transaction::create([
+        //     'user_id' => User::all()->random()->id,
+        //     'psychologist_id' => Psychologist::all()->random()->id,
+        //     'schedule_id' => Schedule::all()->random()->id,
+        //     'payment_type_id' => PaymentType::all()->random()->id,
+        //     'price' => 100000,
+        //     'detail' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit, illum optio. Enim autem iste qui voluptatem eum beatae suscipit mollitia ipsum nam molestiae ex, sint culpa porro provident rerum dolor?',
+        //     'status' => 'done',
+        //     'time' => '2022-05-27'
+        // ]);
     }
 }
