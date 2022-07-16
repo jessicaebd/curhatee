7a13dc63f50add705cdc1401d3826ce12fdd91ad
raynardtjhang <raytjhang@gmail.com>
2022-07-16 22:16:36 +0700

    style

--- resources/views/chat/message.blade.php
+++ resources/views/chat/message.blade.php
@@ -1,28 +1,28 @@
 @foreach ($chats as $chat)
-    <div class="message px-3 pt-3">
+    <div class="message px-3 py-2">
         <div class="message-header">
             <div class="message-header-left">
                 @if ($chat->user_id != null)
                     <img src="{{ asset('storage/images/users/' . $transaction->user->image) }}" alt="profile picture"
                         style="width: 40px; height: 40px; overflow: hidden;">
-                    {{ $chat->user->name }}
+                    <b>{{ $chat->user->name }}</b>
                 @elseif($chat->psychologist_id != null)
                     <img src="{{ asset('storage/images/psychologists/' . $transaction->psychologist->image) }}"
                         alt="profile picture" style="width: 40px; height: 40px; overflow: hidden;">
-                    {{ $chat->psychologist->name }}
+                    <b>{{ $chat->psychologist->name }}</b>
                 @endif
                 --
                 {{ \Carbon\Carbon::parse($chat->sent_at)->format('l, d F Y @ H:i') }}
             </div>
         </div>
         <div class="message-body">
-            <p>{{ $chat->message }}</p>
+            <p class="mb-0">{{ $chat->message }}</p>
         </div>
         @if ($chat->image != null)
             {{ $chat->image }}
             <img src="{{ asset('storage/images/chat/' . $chat->image) }}" alt="image">
         @endif
-        <hr class="mt-3 mb-3" />
+        <hr class="my-0" />
     </div>
 @endforeach
 
