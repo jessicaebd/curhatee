178b15d7f5aacea6af63a4a7924082bdf5f465e7
jessicaebd <jessica.natalia23oct@gmail.com>
2022-07-16 21:46:10 +0700

    home ui

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,85 +1,252 @@
 @extends('layouts.main')
 
+<link rel="stylesheet" href="{{ asset('css/home.css') }}">
+
 @section('title', 'Home')
 
 @section('content')
-    <div class="container mt-5">
+    <div class="container nt-5">
         @if (session('status'))
             <div class="alert alert-success alert-dismissible fade show" role="alert">
                 {{ session('status') }}
                 <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
             </div>
         @endif
+    </div>
 
-        <div class="row">
-            <div class="col-md-3">
-                <a href="/consultation/psychologists">
-                    <div class="card home-menu" style="width: 18rem;">
-                        <img src="{{ asset('storage/images/menu/consultation.png') }}" class="card-img-top">
-                        <div class="card-body">
-                            <h5 class="card-title text-center">@lang('home.consultation')</h5>
+    {{-- Hero --}}
+    <section id="hero">
+        <div class="container z-999">
+            <div class="row justify-content-between">
+                <div class="col-lg-7 pt-5 pt-lg-0 order-2 order-lg-1 d-flex align-items-center">
+                    <div data-aos="zoom-out" class="z-6">
+                        <h1>Find your best psychologist with <span>Curhatee</span></h1>
+                        <h2>Make a better world with a better mental health awareness</h2>
+                        <div class="text-center text-lg-start">
+                            <a href="#about" class="btn-get-started scrollto fw-bolder">Get Started</a>
                         </div>
                     </div>
-                </a>
-            </div>
-
-            <div class="col-md-3">
-                <a href="/forum">
-                    <div class="card home-menu" style="width: 18rem;">
-                        <img src="{{ asset('storage/images/menu/forum.png') }}" class="card-img-top">
-                        <div class="card-body">
-                            <h5 class="card-title text-center">@lang('home.forum')</h5>
-                        </div>
-                    </div>
-                </a>
-            </div>
-
-            <div class="col-md-3">
-                <a href="/diary">
-                    <div class="card home-menu" style="width: 18rem;">
-                        <img src="{{ asset('storage/images/menu/diary.png') }}" class="card-img-top">
-                        <div class="card-body">
-                            <h5 class="card-title text-center">@lang('home.diary')</h5>
-                        </div>
-                    </div>
-                </a>
-            </div>
-
-            <div class="col-md-3">
-                <a href="/podcast">
-                    <div class="card home-menu" style="width: 18rem;">
-                        <img src="{{ asset('storage/images/menu/podcast.png') }}" class="card-img-top">
-                        <div class="card-body">
-                            <h5 class="card-title text-center">@lang('home.podcast')</h5>
-                        </div>
-                    </div>
-                </a>
-            </div>
-        </div>
-
-        {{-- Filter Button choose between Article and Video --}}
-
-        <div class="mt-5 d-flex justify-content-center">
-            <div class="row px-0 py-2 d-flex justify-content-between shadow-sm rounded-pill"
-                style="background-color: #00000005">
-                <div class="" style="width: 20rem;">
-                    <button class="btn btn-light text-primary shadow-sm btn-block rounded-pill" type="button"
-                        data-bs-toggle="collapse" data-bs-target="#filter-article" aria-expanded="false"
-                        aria-controls="filter-article" style="width: 100%">
-                        <b>@lang('home.article')</b>
-                    </button>
                 </div>
-                <div class="" style="width: 20rem;">
-                    <button class="btn text-secondary btn-block rounded-pill" type="button" data-bs-toggle="collapse"
-                        data-bs-target="#filter-video" aria-expanded="false" aria-controls="filter-video"
-                        style="width: 100%">
-                        @lang('home.video')
-                    </button>
+                <div class="col-lg-4 order-1 order-lg-2 hero-img" data-aos="zoom-out" data-aos-delay="300">
+                    <img src="{{ asset('storage/images/hero-img.png') }}" class="img-fluid animated" alt="">
                 </div>
             </div>
         </div>
 
+        <svg class="hero-waves" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
+            viewBox="0 24 150 28 " preserveAspectRatio="none">
+            <defs>
+                <path id="wave-path" d="M-160 44c30 0 58-18 88-18s 58 18 88 18 58-18 88-18 58 18 88 18 v44h-352z">
+            </defs>
+            <g class="wave1">
+                <use xlink:href="#wave-path" x="50" y="3" fill="rgba(255,255,255, .1)">
+            </g>
+            <g class="wave2">
+                <use xlink:href="#wave-path" x="50" y="0" fill="rgba(255,255,255, .2)">
+            </g>
+            <g class="wave3">
+                <use xlink:href="#wave-path" x="50" y="9" fill="#fff">
+            </g>
+        </svg>
+    </section>
+    <!-- End Hero -->
 
-        @include('article.index')
+    {{-- About --}}
+    <section id="about" class="about">
+        <div class="container-fluid">
 
-    @endsection
+            <div class="row">
+                <div class="col-xl-5 col-lg-6 video-box d-flex justify-content-center align-items-center"
+                    data-aos="fade-right">
+                    <img src="{{ asset('storage/images/about-img.png') }}" alt=""
+                        style="height: 500px; width: 500px;">
+                </div>
+
+                <div class="col-xl-7 col-lg-6 icon-boxes d-flex flex-column align-items-stretch justify-content-center py-5 px-lg-5"
+                    data-aos="fade-left">
+                    <h3>Services for Your Mental Health</h3>
+                    <p class="text-muted">What does Curhatee offer?</p>
+
+                    <div class="icon-box" data-aos="zoom-in" data-aos-delay="100">
+                        <div class="icon"><i class="bi bi-balloon-heart"></i></div>
+                        <h4 class="title"><a href="">Booking for consultation</a></h4>
+                        <p class="description text-muted">Offers a wide list of psychologists with various price ranges
+                            according to
+                            your needs</p>
+                    </div>
+
+                    <div class="icon-box" data-aos="zoom-in" data-aos-delay="200">
+                        <div class="icon"><i class="bi bi-chat-left-text"></i></div>
+                        <h4 class="title"><a href="">Discuss on the forum</a></h4>
+                        <p class="description text-muted">Discuss and share stories about mental health with each other in
+                            the forum
+                        </p>
+                    </div>
+
+                    <div class="icon-box" data-aos="zoom-in" data-aos-delay="300">
+                        <div class="icon"><i class="bi bi-chat-heart"></i></div>
+                        <h4 class="title"><a href="">Consultation by live chat</a></h4>
+                        <p class="description text-muted">Don't have time to go to a psychologist? You can easily consult
+                            online</p>
+                    </div>
+
+                </div>
+            </div>
+
+        </div>
+    </section><!-- End About Section -->
+
+    {{-- Features --}}
+    <section id="features" class="features">
+        <div class="container">
+
+            <div class="section-title" data-aos="fade-up">
+                <h2>Features</h2>
+                <p>Check The Features</p>
+            </div>
+
+            <div class="row" data-aos="fade-left">
+                <div class="col-md-4">
+                    <div class="icon-box" data-aos="zoom-in" data-aos-delay="50">
+                        <i class="bi bi-balloon-heart me-2" style="color: #ffbb2c; font-size:24px;"></i>
+                        <h3><a href="/consultation/psychologists">Consultation</a></h3>
+                    </div>
+                </div>
+                <div class="col-md-4 mt-4 mt-md-0">
+                    <div class="icon-box" data-aos="zoom-in" data-aos-delay="100">
+                        <i class="bi bi-chat-left-text me-2" style="color: #5578ff; font-size:22px;"></i>
+                        <h3><a href="/forum">Forum</a></h3>
+                    </div>
+                </div>
+                <div class="col-md-4 mt-4 mt-md-0">
+                    <div class="icon-box" data-aos="zoom-in" data-aos-delay="150">
+                        <i class="bi bi-chat-heart me-2" style="color: #e80368; font-size:24px;"></i>
+                        <h3><a href="/article">Article</a></h3>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </section>
+    <!-- End Features Section -->
+
+    {{-- Counts --}}
+    <section id="counts" class="counts">
+        <div class="container">
+
+            <div class="row" data-aos="fade-up">
+
+                <div class="col-lg-3 col-md-6">
+                    <div class="count-box">
+                        <i class="bi bi-emoji-smile"></i>
+                        <span data-purecounter-start="0" data-purecounter-end="232" data-purecounter-duration="1"
+                            class="purecounter"></span>
+                        <p>Happy Clients</p>
+                    </div>
+                </div>
+
+                <div class="col-lg-3 col-md-6 mt-5 mt-lg-0">
+                    <div class="count-box">
+                        <i class="bi bi-people"></i>
+                        <span data-purecounter-start="0" data-purecounter-end="15" data-purecounter-duration="1"
+                            class="purecounter"></span>
+                        <p>Trusted Psychologist</p>
+                    </div>
+                </div>
+
+                <div class="col-lg-3 col-md-6 mt-5 mt-lg-0">
+                    <div class="count-box">
+                        <i class="bi bi-headset"></i>
+                        <span data-purecounter-start="0" data-purecounter-end="1463" data-purecounter-duration="1"
+                            class="purecounter"></span>
+                        <p>Hours Of Support</p>
+                    </div>
+                </div>
+
+                <div class="col-lg-3 col-md-6 mt-5 mt-md-0">
+                    <div class="count-box">
+                        <i class="bi bi-journal-richtext"></i>
+                        <span data-purecounter-start="0" data-purecounter-end="521" data-purecounter-duration="1"
+                            class="purecounter"></span>
+                        <p>Transaction History</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- End Counts Section -->
+
+    {{-- Contact --}}
+    <section id="contact" class="contact">
+        <div class="container">
+
+            <div class="section-title" data-aos="fade-up">
+                <h2>Contact</h2>
+                <p>Contact Us</p>
+            </div>
+
+            <div class="row">
+
+                <div class="col-lg-4" data-aos="fade-right" data-aos-delay="100">
+                    <div class="info">
+                        <div class="address">
+                            <i class="bi bi-geo-alt"></i>
+                            <h4>Location:</h4>
+                            <p>Jalan Suka Suka, Jakarta</p>
+                        </div>
+
+                        <div class="email">
+                            <i class="bi bi-envelope"></i>
+                            <h4>Email:</h4>
+                            <p>curhatee@example.com</p>
+                        </div>
+
+                        <div class="phone">
+                            <i class="bi bi-phone"></i>
+                            <h4>Call:</h4>
+                            <p>+62 822 02320392</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-lg-8 mt-5 mt-lg-0" data-aos="fade-left" data-aos-delay="200">
+                    <form action="" method="post" role="form" class="php-email-form">
+                        <div class="row">
+                            <div class="col-md-6 form-group">
+                                <input type="text" name="name" class="form-control" id="name"
+                                    placeholder="Your Name" required>
+                            </div>
+                            <div class="col-md-6 form-group mt-3 mt-md-0">
+                                <input type="email" class="form-control" name="email" id="email"
+                                    placeholder="Your Email" required>
+                            </div>
+                        </div>
+                        <div class="form-group mt-3">
+                            <input type="text" class="form-control" name="subject" id="subject"
+                                placeholder="Subject" required>
+                        </div>
+                        <div class="form-group mt-3">
+                            <textarea class="form-control" name="message" rows="5" placeholder="Message" required></textarea>
+                        </div>
+                        <div class="my-3">
+                            <div class="loading">Loading</div>
+                            <div class="error-message"></div>
+                            <div class="sent-message">Your message has been sent. Thank you!</div>
+                        </div>
+                        <div class="text-center"><button type="submit">Send Message</button></div>
+                    </form>
+
+                </div>
+
+            </div>
+
+        </div>
+    </section>
+    <!-- End Contact Section -->
+
+    <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i
+            class="bi bi-arrow-up-short"></i></a>
+    <div id="preloader"></div>
+
+    <script src="{{ asset('js/home.js') }}"></script>
+@endsection
