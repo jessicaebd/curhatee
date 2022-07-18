aa104ef91d8eeef1b1d841bdc9d5cf7bd1028462
fredericsetievi <fredericsetievi@gmail.com>
2022-07-18 09:46:17 +0700

    fix article show detail

--- resources/views/forum/index.blade.php
+++ resources/views/forum/index.blade.php
@@ -4,45 +4,18 @@
 
 @section('css')
     <style>
-        .message-container {
-            float: left;
-            width: 1000px;
-            overflow-y: scroll;
-            height: 700px;
-            background-color: yellow;
-        }
-
-        .btn {
-            background-color: #dbdbdb;
-            border-color: #bbb;
-            color: #666;
-        }
-
-        .btn:hover,
-        .btn.active {
-            text-shadow: 0 1px 0 #b12f27;
-            background-color: #f64136;
-            border-color: #b12f27;
-        }
-
-        .btn:active {
-            box-shadow: 0 0 5px 3px rgba(0, 0, 0, 0.2) inset;
-        }
-
-        .btn span {
-            color: #f64136;
-        }
-
-        .btn:hover,
-        .btn:hover span,
-        .btn.active,
-        .btn.active span {
-            color: #eeeeee;
-        }
-
-        .btn:active span {
-            color: #b12f27;
-            text-shadow: 0 1px 0 rgba(255, 255, 255, 0.3);
+        .btn-like {
+            box-shadow: 1px 1px 0 rgba(255, 255, 255, 0.5) inset;
+            border-radius: 3px;
+            border: 1px solid;
+            display: inline-block;
+            height: 18px;
+            line-height: 18px;
+            padding: 0 8px;
+            position: relative;
+            font-size: 12px;
+            text-decoration: none;
+            text-shadow: 0 1px 0 rgba(255, 255, 255, 0.5);
         }
     </style>
 @endsection
@@ -61,8 +34,20 @@
         <hr class="mt-3 mb-3" />
 
         @foreach ($forums as $forum)
-            {{ $forum->title }} --- {{ \Carbon\Carbon::parse($forum->created_at)->format('l, d F Y @ H:i') }} ---
-            <i class="bi bi-heart"> {{ $forum->likes }}likes</i>
+            {{ $forum->title }} --- <i
+                class="bi bi-clock me-2"></i>{{ \Carbon\Carbon::parse($forum->created_at)->format('l, d F Y - H:i') }} ---
+
+
+            {{-- like button --}}
+            {{-- BOLEH LIAT REFERENSI INI STYLING LIKE BUTTONNY: https://codepen.io/Idered/pen/ALYLaM --}}
+            <form action="{{ route('like_forum', $forum->id) }}" method="post">
+                @csrf
+                <button type="submit"
+                    class="btn-like {{ $forum->is_forum_liked ? 'btn-danger' : 'btn-outline-danger' }}"><i
+                        class="bi bi-heart"></i>
+                    {{ $forum->likes }}
+                    likes</button>
+            </form>
 
             <br>
             @if ($forum->user_id != null)
