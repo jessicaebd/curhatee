2bdb44198adadeb3f7a1556102f579f6c870375c
raynardtjhang <raytjhang@gmail.com>
2022-07-20 19:30:08 +0700

    fix lang

---
+++ resources/lang/en/main_admin.php
@@ -0,0 +1,16 @@
+<?php
+    return [
+        'dashboard' => 'Dashboard',
+        'articles' => 'Articles',
+        'hospitals' => 'Hospitals',
+        'podcast' => 'Podcasts',
+        'psychologist' => 'Psychologist',
+        'schedules' => 'Schedules',
+        'users' => 'Users',
+        'saved_reports' => 'Saved Reports',
+        'current_month' => 'Current Month',
+        'last_quarter' => 'Last Quarter',
+        'social_engagement' => 'Social Engagement',
+        'year-end_sale' => 'Year-end Sale'
+    ];
+?>
