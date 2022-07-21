417870c578fcf322f9cfd7f5085676b369602a40
jessicaebd <jessica.natalia23oct@gmail.com>
2022-07-21 19:16:59 +0700

    save

--- app/Models/ReplyForum.php
+++ app/Models/ReplyForum.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use App\Models\User;
 use App\Traits\Uuid;
 use App\Models\Forum;
-use App\Models\ReplyForum;
+// use App\Models\ReplyForum;
 use App\Models\LikedReplyForum;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
