94c64f053276e710a1da635235fedcbf6070fa7e
AndreasRonal <andreas.ronal@binus.ac.id>
2022-07-23 12:30:48 +0700

    schedule admin

--- app/Http/Controllers/ScheduleController.php
+++ app/Http/Controllers/ScheduleController.php
@@ -33,7 +33,7 @@ class ScheduleController extends Controller
         $data = [
             'psychologist' => Psychologist::findOrFail($id),
             'schedules' => Schedule::where('psychologist_id', $id)->get(),
-            'schedules_time' => ['09:00:00','10:00:00','11:00:00','12:00:00','13:00:00','14:00:00','15:00:00','16:00:00','17:00:00'],
+            'schedules_time' => ['08:00:00','09:00:00','10:00:00','11:00:00','12:00:00','13:00:00','14:00:00','15:00:00','16:00:00'],
             'days' => ['Monday','Tuesday','Wednesday','Thursday','Friday','Saturday','Sunday'],
         ];
 
--- resources/views/admin/schedule/view.blade.php
+++ resources/views/admin/schedule/view.blade.php
@@ -3,7 +3,7 @@
 @section('content')
     <div class="container py-5 h-100">
         <div class="row justify-content-center align-items-center h-100">
-            <div class="col-12 col-lg-11 col-xl-11">
+            <div class="col-12 col-lg-12 col-xl-12">
                 <div class="card shadow-2-strong card-registration" style="border-radius: 15px;">
                     <div class="card-body p-4 p-md-5">
                         <h3 class="fw-bolder">Schedule for {{ $psychologist->name }}</h3>
@@ -21,8 +21,8 @@
                             </div>
                         @endif
 
-                        <div class="row">
-                            <table id="example" class="table table-striped" style="width:100%">
+                        <div class="row table-responsive">
+                            <table class="table table-bordered" style="width:100%">
                                 <thead>
                                     <tr>
                                         <th>Hour</th>
@@ -40,153 +40,45 @@
                                         <tr>
                                             <th>{{ \Carbon\Carbon::createFromFormat('H:i:s', $time)->format('H:i') }} - {{ \Carbon\Carbon::createFromFormat('H:i:s', $time)->addHours(1)->format('H:i') }}</th>
                                             @foreach ($days as $day)
-                                                @foreach($schedules as $schedule)
-                                                    <td>
-                                                        test
-                                                    </td>
-                                                @endforeach
+                                                <td>
+                                                    @foreach ($schedules as $schedule)
+                                                        @if ($schedule->day == $day && \Carbon\Carbon::createFromFormat('Y-m-d H:i:s', $schedule->startTime)->format('H:i:s') == $time)
+                                                            @if ($schedule->isActive == true)
+                                                                <div class="d-flex justify-content-between">
+                                                                    <div class="text-white bg-success text-center mx-2 border" style="min-width:75px; border-radius: 8px;">
+                                                                        <span class="align-middle">Active</span>
+                                                                    </div>
+                                                                    <form action="{{ route('update_schedule', $schedule->id) }}" method="post">
+                                                                        @csrf
+                                                                        @method('PUT')
+                                                                        <button type="submit" class="btn btn-sm btn-danger">
+                                                                            <i class="bi bi-x-circle"></i>
+                                                                        </button>
+                                                                    </form>
+                                                                </div>
+                                                                @continue
+                                                            @else
+                                                                <div class="d-flex justify-content-between">
+                                                                    <div class="text-white bg-danger text-center mx-2 border" style="min-width:75px; border-radius: 8px;">
+                                                                        <span class="align-middle">Inactive</span>
+                                                                    </div>
+                                                                    <form action="{{ route('update_schedule', $schedule->id) }}" method="post">
+                                                                        @csrf
+                                                                        @method('PUT')
+                                                                        <button type="submit" class="btn btn-sm btn-success">
+                                                                            <i class="bi bi-check-circle"></i>
+                                                                        </button>
+                                                                    </form>
+                                                                </div>
+                                                            @endif
+                                                        @endif
+                                                    @endforeach
+                                                </td>
                                             @endforeach
-                                            <td>
-                                            {{-- @foreach ($schedules as $schedule)
-                                                @for ($i = 0; $i < count($schedule_days); $i++)
-                                                    @for ($j = 0; $j < count($schedule_time); $j++)
-
-                                                    @endfor
-                                                @endfor
-                                                @if ($schedule->day == 'Monday' && \Carbon\Carbon::createFromFormat('Y-m-d H:i:s', $schedule->startTime)->format('H:i:s') == $schedule_time[$i])
-                                                    @if ($schedule->isActive == true)
-                                                        <div class="bg-success">
-                                                            <span class="text-black">Active</span>
-                                                            <form action="{{ route('update_schedule', $schedule->id) }}" method="post">
-                                                                @csrf
-                                                                @method('PUT')
-                                                                <button type="submit" class="btn btn-sm btn-danger">
-                                                                    <i class="bi bi-x-circle"></i>
-                                                                </button>
-                                                            </form>
-                                                        </div>
-                                                    @else
-                                                        <div class="bg-danger">
-                                                            <span class="text-white">Inactive</span>
-                                                        </div>
-                                                    @endif
-                                                @endif
-                                            @endforeach --}}
-                                            test
-                                            </td>
                                         </tr>
                                     @endforeach
-                                    <tr>
-                                        <th>09.00-10.00</th>
-                                        <td>
-                                        {{-- @foreach ($schedules as $schedule)
-                                            @for ($i = 0; $i < count($schedule_days); $i++)
-                                                @for ($j = 0; $j < count($schedule_time); $j++)
-
-                                                @endfor
-                                            @endfor
-                                            @if ($schedule->day == 'Monday' && \Carbon\Carbon::createFromFormat('Y-m-d H:i:s', $schedule->startTime)->format('H:i:s') == $schedule_time[])
-                                                @if ($schedule->isActive == true)
-                                                    <div class="bg-success">
-                                                        <span class="text-black">Active</span>
-                                                        <form action="{{ route('update_schedule', $schedule->id) }}" method="post">
-                                                            @csrf
-                                                            @method('PUT')
-                                                            <button type="submit" class="btn btn-sm btn-danger">
-                                                                <i class="bi bi-x-circle"></i>
-                                                            </button>
-                                                        </form>
-                                                    </div>
-                                                @else
-                                                    <div class="bg-danger">
-                                                        <span class="text-white">Inactive</span>
-                                                    </div>
-                                                @endif
-                                            @endif
-                                        @endforeach --}}
-                                        </td>
-                                    </tr>
-                                    <tr>
-                                        <th>10.00-11.00</th>
-                                        <td>Accountant</td>
-                                        <td>Tokyo</td>
-                                        <td>63</td>
-                                        <td>2011-07-25</td>
-                                        <td>$170,750</td>
-                                    </tr>
-                                    <tr>
-                                        <th>11.00-12.00</th>
-                                        <td>Junior Technical Author</td>
-                                        <td>San Francisco</td>
-                                        <td>66</td>
-                                        <td>2009-01-12</td>
-                                        <td>$86,000</td>
-                                    </tr>
-                                    <tr>
-                                        <th>12.00-13.00</th>
-                                        <td>Senior Javascript Developer</td>
-                                        <td>Edinburgh</td>
-                                        <td>22</td>
-                                        <td>2012-03-29</td>
-                                        <td>$433,060</td>
-                                    </tr>
-                                    <tr>
-                                        <th>13.00-14.00</th>
-                                        <td>Accountant</td>
-                                        <td>Tokyo</td>
-                                        <td>33</td>
-                                        <td>2008-11-28</td>
-                                        <td>$162,700</td>
-                                    </tr>
-                                    <tr>
-                                        <th>14.00-15.00</th>
-                                        <td>Integration Specialist</td>
-                                        <td>New York</td>
-                                        <td>61</td>
-                                        <td>2012-12-02</td>
-                                        <td>$372,000</td>
-                                    </tr>
-                                    <tr>
-                                        <th>15.00-16.00</th>
-                                        <td>Sales Assistant</td>
-                                        <td>San Francisco</td>
-                                        <td>59</td>
-                                        <td>2012-08-06</td>
-                                        <td>$137,500</td>
-                                    </tr>
-                                    <tr>
-                                        <th>16.00-17.00</th>
-                                        <td>Integration Specialist</td>
-                                        <td>Tokyo</td>
-                                        <td>55</td>
-                                        <td>2010-10-14</td>
-                                        <td>$327,900</td>
-                                    </tr>
-                                    <tr>
-                                        <th>17.00-18.00</th>
-                                        <td>Javascript Developer</td>
-                                        <td>San Francisco</td>
-                                        <td>39</td>
-                                        <td>2009-09-15</td>
-                                        <td>$205,500</td>
-                                    </tr>
                                 </tbody>
-                                <tfoot>
-                                    <tr>
-                                        <th>Name</th>
-                                        <th>Position</th>
-                                        <th>Office</th>
-                                        <th>Age</th>
-                                        <th>Start date</th>
-                                        <th>Salary</th>
-                                    </tr>
-                                </tfoot>
                             </table>
-                        </div>
-
-
-                        <div class="d-flex justify-content-center mt-5 pt-2">
-                            <input class="btn btn-primary btn-lg px-5" type="submit" value="Submit" />
                         </div>
                     </div>
                 </div>
