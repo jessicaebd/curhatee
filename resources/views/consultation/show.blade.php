d43e0628ff35892444927bacdae6e0ccfdd18243
jessicaebd <jessica.natalia23oct@gmail.com>
2022-07-23 14:10:37 +0700

    up

--- resources/views/consultation/show.blade.php
+++ resources/views/consultation/show.blade.php
@@ -51,8 +51,10 @@
         </svg>
     </section>
 
-    <div class="container">
-        <div class="row">
+    <div class="container my-3">
+
+
+        <div class="row mb-4">
             @if (request('date'))
                 {{-- Choose Hours --}}
                 <h5>@lang('show_consultation.consultation_for')<b>{{ $date->format('l, d F Y') }}</b></h5>
@@ -83,7 +85,7 @@
                         <label for="hours" class="form-label fw-bolder">@lang('show_consultation.choose_hours')</label>
                         <div class="row">
                             @foreach ($schedules as $schedule)
-                                <div class="col-md-1">
+                                <div class="col-md-2">
                                     <div class="mb-3">
                                         {{ $schedule->status }}
                                         <input type="radio" class="btn-check btn-green" name="schedule"
@@ -167,5 +169,38 @@
                 @endfor
             @endif
         </div>
+
+        <div class="row about-hospital mb-4">
+            <div class="border-bottom border-1 border-dark">
+                <a class="link-dark" data-bs-toggle="collapse" href="#hospitalDescription" role="button"
+                    aria-expanded="false" aria-controls="hospitalDescription">
+                    <div class="d-flex justify-content-between">
+                        <span class="fw-bold">About Hospital</span>
+                        <i class="bi bi-caret-down fw-bold"></i>
+                    </div>
+                </a>
+            </div>
+            <div class="collapse" id="hospitalDescription">
+                <div class="card-body">
+                    <h5 class="fw-bold l-font">{{ $psychologist->hospital->name }}</h5>
+
+                    <div class="d-flex">
+                        <div class="col-md-5 me-3">
+                            <img src="{{ asset('storage/images/hospitals/' . $psychologist->hospital->image) }}"
+                                alt="" style="width: 100%;height: 250px;">
+                        </div>
+                        <div class="col-md-7 s-font text-justify pe-5 ps-3 my-auto">
+                            <span class="s-font fw-bolder">Address: <br> {{ $psychologist->hospital->address }}</span>
+                            <br><br>
+                            <span class="s-font fw-bolder">Contact: <br> {{ $psychologist->hospital->contact }}</span>
+
+                        </div>
+                    </div>
+                    <div class="text-secondary mt-3 s-font text-justify">{!! $psychologist->hospital->description !!}</div>
+                </div>
+            </div>
+        </div>
+
+        <div class="row doctor-review"></div>
     </div>
 @endsection
