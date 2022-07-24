0a4e4361490e2901d58277c24937b03e5969a91a
jessicaebd <jessica.natalia23oct@gmail.com>
2022-07-24 11:22:41 +0700

    create forum

--- resources/views/forum/add.blade.php
+++ resources/views/forum/add.blade.php
@@ -1,49 +1,63 @@
 @extends($view == 'Psychologist' ? 'layouts.main-psychologist' : 'layouts.main')
 
-@section('title', 'Add Forum')
+@section('title', 'Create New Forum')
 
 @section('content')
-    <h1>Add Forum</h1>
-    <br>
+    <div class="containter mt-5 pt-5">
+        <div class="row d-flex justify-content-center">
+            <div class="col-md-10">
+                <div class="card">
+                    <div class="card-header">
+                        <h3 class="card-title">Create New Forum</h3>
+                    </div>
 
-    <form
-        action="{{ Auth::guard('webpsychologist')->user() != null
-            ? route('create_forum_psychologist')
-            : route('create_forum_user') }}"
-        method="post" enctype="multipart/form-data">
-        @csrf
-        <div class="row mb-3">
-            <label for="title" class="col-md-4 col-form-label text-md-end">Title Topic</label>
-            <div class="col-md-6">
-                <input id="title" type="text" class="form-control" name="title" value="{{ old('title') }}" required
-                    autocomplete="title">
+                    <div class="card-body">
+                        <form
+                            action="{{ Auth::guard('webpsychologist')->user() != null
+                                ? route('create_forum_psychologist')
+                                : route('create_forum_user') }}"
+                            method="post" enctype="multipart/form-data">
+                            @csrf
+                            <div class="row mb-4">
+                                <div class="col-md-12">
+                                    <input id="title" type="text" class="form-control" name="title"
+                                        value="{{ old('title') }}" required autocomplete="title"
+                                        placeholder="Enter your forum title here...">
+                                </div>
+                            </div>
+
+                            <div class="row mb-3">
+                                <input id="content" type="hidden" name="content" placeholder="Content">
+                                <trix-editor input="content" placeholder="Enter your content here..."></trix-editor>
+                            </div>
+                            <div class="row mb-4">
+                                <div class="col-md-4">
+                                    <input type="file" class="form-control" id="image" name="image"
+                                        placeholder="Choose your profile image">
+                                </div>
+                                <div class="col-md-2">
+                                    <label for="image" class="col-form-label text-md-end m-font text-muted">
+                                        (Optional)</label>
+                                </div>
+                            </div>
+
+                            {{-- Error Message --}}
+                            @if ($errors->any())
+                                <div class="alert alert-danger">
+                                    {{ $errors->first() }}
+                                </div>
+                            @endif
+
+                            {{-- Submit Button --}}
+                            <div class="d-flex justify-content-end">
+                                <button type="submit" class="btn btn-primary">Add Forum</button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
             </div>
         </div>
-        <div class="row mb-3">
-            <label for="content" class="col-md-4 col-form-label text-md-end">Isi Content</label>
-            {{-- <textarea class="form-control" name="content" id="content" rows="3" placeholder="Type a content..."></textarea> --}}
-            <input id="content" type="hidden" name="content">
-            <trix-editor input="content"></trix-editor>
-        </div>
 
-        <div class="row mb-3">
-            <label for="image" class="col-md-4 col-form-label text-md-end">Add Picture (Optional)</label>
-            <div class="col-md-6">
-                <input type="file" class="form-control" id="image" name="image"
-                    placeholder="Choose your profile image">
-            </div>
-        </div>
 
-        {{-- Error Message --}}
-        @if ($errors->any())
-            <div class="alert alert-danger">
-                {{ $errors->first() }}
-            </div>
-        @endif
-
-        {{-- Submit Button --}}
-        <div class="text-center">
-            <button type="submit" class="btn btn-primary">Add Forum</button>
-        </div>
-    </form>
+    </div>
 @endsection
