bb04ad861b0787453291e9c07abdb656640f683f
AndreasRonal <andreas.ronal@binus.ac.id>
2022-07-24 16:45:51 +0700

    edit profile psikolog

--- resources/views/profile/psychologist/index.blade.php
+++ resources/views/profile/psychologist/index.blade.php
@@ -23,33 +23,159 @@
     <br>
     {{ $psychologist->description }}
     <br>
-    ===========
-    <br>
-    @lang('profile.total schedule in a week') {{ $psychologist->schedule->count() }}
-    <br>
-    @lang('profile.total transaction') {{ $psychologist->transaction->count() }}
-    <br>
-    @lang('profile.total online consultation')
-    {{ $psychologist->transaction->where('consultation_type_id', $online_consultation_id)->count() }}
-    <br>
-    @lang('profile.total offline consultation')
-    {{ $psychologist->transaction->where('consultation_type_id', $offline_consultation_id)->count() }}
-    <br>
-    @lang('profile.total transaction pending') {{ $psychologist->transaction->where('status', 'Pending')->count() }}
-    <br>
-    @lang('profile.total transaction confirmed') {{ $psychologist->transaction->where('status', 'Confirmed')->count() }}
-    <br>
-    @lang('profile.total transaction success') {{ $psychologist->transaction->where('status', 'Finished')->count() }}
-    <br>
-    @lang('profile.total transaction rejected') {{ $psychologist->transaction->where('status', 'Rejected')->count() }}
-    <br>
-    @lang('profile.total review') {{ $psychologist->review->count() }}
-    <br>
-    {{ $psychologist->rating }}
-    <br>
-    @lang('profile.total revenue') {{ $psychologist->transaction->where('status', '!=', 'Rejected')->sum('price') }}
 
+    <div class="content">
+        <div class="my_dashboard row mb-4">
+            <div class="col-12">
+                <h2 class="nav-title l-font border-bottom mb-3">Users</h2>
+                {{-- <div class="row">
+                    <div class="col-md-3">
+                        <div class="card shadow-sm mb-3">
+                            <div class="card-body text-center">
+                                <h5 class="card-title fw-bold m-font">Total Users</h5>
+                                <p class="card-text l-font text-muted fw-bolder">
+                                    {{ $users->count() }}</p>
+                            </div>
+                        </div>
+                    </div>
 
+                    <div class="col-md-3">
+                        <div class="card shadow-sm mb-3">
+                            <div class="card-body text-center">
+                                <h5 class="card-title fw-bold m-font">Total Psychologists</h5>
+                                <p class="card-text l-font text-muted fw-bolder">{{ $psychologists->count() }}
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div> --}}
+
+                <h2 class="nav-title l-font border-bottom mb-3">Transaction</h2>
+                <div class="row">
+
+                    <div class="col-md-3">
+                        <div class="card shadow-sm mb-3">
+                            <div class="card-body text-center">
+                                <h5 class="card-title fw-bold m-font">@lang('profile.total schedule in a week')</h5>
+                                <p class="card-text l-font text-muted fw-bolder">
+                                    {{ $psychologist->schedule->count() }} </p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-md-3">
+                        <div class="card shadow-sm mb-3">
+                            <div class="card-body text-center">
+                                <h5 class="card-title fw-bold m-font">@lang('profile.total transaction') </h5>
+                                <p class="card-text l-font text-muted fw-bolder"> {{ $psychologist->transaction->count() }}
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-md-3">
+                        <div class="card shadow-sm mb-3">
+                            <div class="card-body text-center">
+                                <h5 class="card-title fw-bold m-font">@lang('profile.total online consultation')</h5>
+                                <p class="card-text l-font text-muted fw-bolder">
+                                    {{ $psychologist->transaction->where('consultation_type_id', $online_consultation_id)->count() }}
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-md-3">
+                        <div class="card shadow-sm mb-3">
+                            <div class="card-body text-center">
+                                <h5 class="card-title fw-bold m-font">@lang('profile.total offline consultation')</h5>
+                                <p class="card-text l-font text-muted fw-bolder">
+                                    {{ $psychologist->transaction->where('consultation_type_id', $offline_consultation_id)->count() }}
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <h2 class="nav-title l-font border-bottom mb-3">Transaction by Status</h2>
+                <div class="row">
+                    <div class="col-md-3">
+                        <div class="card shadow-sm mb-3">
+                            <div class="card-body text-center">
+                                <h5 class="card-title fw-bold m-font">@lang('profile.total transaction pending')</h5>
+                                <p class="card-text l-font text-muted fw-bolder">
+                                    {{ $psychologist->transaction->where('status', 'Pending')->count() }}
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-md-3">
+                        <div class="card shadow-sm mb-3">
+                            <div class="card-body text-center">
+                                <h5 class="card-title fw-bold m-font">@lang('profile.total transaction confirmed')</h5>
+                                <p class="card-text l-font text-muted fw-bolder">
+                                    {{ $psychologist->transaction->where('status', 'Confirmed')->count() }}
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-md-3">
+                        <div class="card shadow-sm mb-3">
+                            <div class="card-body text-center">
+                                <h5 class="card-title fw-bold m-font">@lang('profile.total transaction success')</h5>
+                                <p class="card-text l-font text-muted fw-bolder">
+                                    {{ $psychologist->transaction->where('status', 'Finished')->count() }}</p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-md-3">
+                        <div class="card shadow-sm mb-3">
+                            <div class="card-body text-center">
+                                <h5 class="card-title fw-bold m-font">@lang('profile.total transaction rejected')</h5>
+                                <p class="card-text l-font text-muted fw-bolder">
+                                    {{ $psychologist->transaction->where('status', 'Rejected')->count() }} </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <h2 class="nav-title l-font border-bottom mb-3">Others</h2>
+                <div class="row">
+                    <div class="col-md-3">
+                        <div class="card shadow-sm mb-3">
+                            <div class="card-body text-center">
+                                <h5 class="card-title fw-bold m-font">@lang('profile.total review')</h5>
+                                <p class="card-text l-font text-muted fw-bolder">
+                                    {{ $psychologist->review->count() }} </p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-md-3">
+                        <div class="card shadow-sm mb-3">
+                            <div class="card-body text-center">
+                                <h5 class="card-title fw-bold m-font">Rating</h5>
+                                <p class="card-text l-font text-muted fw-bolder">
+                                    {{ $psychologist->rating }} </p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-md-3">
+                        <div class="card shadow-sm mb-3">
+                            <div class="card-body text-center">
+                                <h5 class="card-title fw-bold m-font">@lang('profile.total revenue')</h5>
+                                <p class="card-text l-font text-muted fw-bolder">
+                                    {{ $psychologist->transaction->where('status', '!=', 'Rejected')->sum('price') }}</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 
     <div class="d-flex justify-content-end">
         <a href="{{ route('psychologist_edit_psychologist', $psychologist->id) }}"
